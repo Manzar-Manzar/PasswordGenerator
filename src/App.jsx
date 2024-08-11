a0dc846b6e16c072d5f1dfeb98d4a5689345e565
Manzar <manzarinemanzar@gmail.com>
2024-08-11 20:53:17 +0545

    Password generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,96 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 
-function App() {
-  const [color, setColor] = useState("pink")
+export default function PasswordGenerator() {
+  const [length, setLength] = useState(10); 
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [password, setPassword] = useState();
+
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    if(charAllowed) str += '`~!@#$%^&*()_+{}[]'
+    if(numberAllowed) str += "0123456789"
+    for(let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * length + 1);
+      pass += str.charAt(char)
+    }
+    setPassword(pass)
+    // If the method/s run/s
+    // setPassword is for the optimization as the useCallback function uses memoization concept
+  }, [length, charAllowed, numberAllowed, setPassword])
+
+  const passwordRef = useRef(null)
+
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select()
+    // Since you are using core react, so you can access it.
+    // When you are on server side rendering, you don't have a window object like nextjs.
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+
+  useEffect(() => {
+    passwordGenerator()
+    // If the value changes
+  }, [length, numberAllowed, charAllowed, passwordGenerator])
+
+  const handleLengthChange = (event) => {
+    setLength(event.target.value);
+  };
+
   return (
-    <div className='w-full h-screen duration-200' style={{backgroundColor: color}}>
-       <button className='bg-red-400 mx-2' onClick={() => setColor("red")}>Red</button>
-       <button className='bg-yellow-400 mx -2' onClick={() => setColor("yellow")}>Yellow</button>
-       <button className='bg-green-400 mx-2' onClick={() => setColor("green")}>Green</button>
-       <button className='bg-blue-400' onClick={() => setColor("blue")}>Blue</button>
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+      <h1 className='text-white text-center my-3'>Password generator</h1>
+    <div className="flex shadow rounded-lg overflow-hidden mb-4">
+        <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="Password"
+            readOnly
+            ref={passwordRef}
+        />
+        <button
+        onClick={copyPasswordToClipboard}
+        className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+        >copy</button>
+        
     </div>
+    <div className='flex text-sm gap-x-2'>
+      <div className='flex items-center gap-x-1'>
+        <input 
+        type="range"
+        min={6}
+        max={100}
+        value={length}
+         className='cursor-pointer'
+         onChange={(e) => {setLength(e.target.value)}}
+          />
+          <label>Length: {length}</label>
+      </div>
+      <div className="flex items-center gap-x-1">
+      <input
+          type="checkbox"
+          defaultChecked={numberAllowed}
+          id="numberInput"
+          onChange={() => {
+              setNumberAllowed((prev) => !prev);
+          }}
+      />
+      <label htmlFor="numberInput">Numbers</label>
+      </div>
+      <div className="flex items-center gap-x-1">
+          <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="characterInput"
+              onChange={() => {
+                  setCharAllowed((prev) => !prev )
+              }}
+          />
+          <label htmlFor="characterInput">Characters</label>
+      </div>
+    </div>
+</div>
   );
 }
-
-export default App;
